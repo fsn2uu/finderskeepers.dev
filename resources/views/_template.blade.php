141f79aa885b262cc9e00a2726a03c22d7c2116c
Kevin Pritchett <fsn2uu@gmail.com>
2022-08-09 08:58:04 -0500

    version for caching

--- resources/views/_template.blade.php
+++ resources/views/_template.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Finders Keepers</title>
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}?v=1.0.3">
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}?v=1.0.4">
     </head>
     <body>
         @include('_nav')
